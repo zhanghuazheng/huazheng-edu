dc7b6f35b7dc7229e05438e302fe77bb1bb26b27
zhanghuazheng <zhanghzi@dicts.com>
2020-07-02 19:32:38 +0800

    1.增加网关服务模块，并注册到服务注册中心

--- gateway/src/main/java/com/huazheng/gateway/GateWayMainApplication.java
+++ gateway/src/main/java/com/huazheng/gateway/GateWayMainApplication.java
@@ -1,4 +1,4 @@
-package com.huazheng.system;
+package com.huazheng.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,18 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @Classname SystemMainApplication
- * @Description 系统应用模块
- * @Date 2020/7/2 17:52
+ * @Classname GateWayMainApplication
+ * @Description 增加网关模块
+ * @Date 2020/7/2 19:16
  * @Created by zhanghuazheng
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class SystemMainApplication {
-    private static final Logger log = LoggerFactory.getLogger(SystemMainApplication.class);
+public class GateWayMainApplication {
+    private static final Logger log = LoggerFactory.getLogger(GateWayMainApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(SystemMainApplication.class,args);
-        log.info("SystemMainApplication 启动成功 ");
+        SpringApplication.run(GateWayMainApplication.class,args);
+        log.info("GateWayMainApplication 启动成功 ");
     }
 }
