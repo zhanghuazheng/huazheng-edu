ef92c488011f423244325e55f1c7f52684cc445a
zhanghuazheng <zhanghzi@dicts.com>
2020-07-02 13:41:14 +0800

    1.改造父子项目； 2.创建eureka服务注册中心

---
+++ eureka-config/src/main/java/com/huazheng/eureka/EurekaMainApplication.java
@@ -0,0 +1,10 @@
+package com.huazheng.eureka;
+
+/**
+ * @Classname EurekaMainApplication
+ * @Description TODO
+ * @Date 2020/7/2 13:39
+ * @Created by zhanghuazheng
+ */
+public class EurekaMainApplication {
+}
