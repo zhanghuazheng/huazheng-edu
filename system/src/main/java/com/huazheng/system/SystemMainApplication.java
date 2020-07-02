4f57018eab4441ec1632a69289a0eb5a229ba180
zhanghuazheng <zhanghzi@dicts.com>
2020-07-02 19:07:42 +0800

    1.将system模块注册到eureka

--- system/src/main/java/com/huazheng/system/SystemMainApplication.java
+++ system/src/main/java/com/huazheng/system/SystemMainApplication.java
@@ -1,11 +1,10 @@
 package com.huazheng.system;
 
-import org.bouncycastle.math.ec.ScaleYPointMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Classname SystemMainApplication
@@ -14,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Created by zhanghuazheng
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemMainApplication {
     private static final Logger log = LoggerFactory.getLogger(SystemMainApplication.class);
 
