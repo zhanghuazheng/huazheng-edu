294f68b64f47af3e468386274b2a06da69abacf0
zhanghuazheng <zhanghzi@dicts.com>
2020-07-02 14:40:51 +0800

    1.改造父子项目； 2.创建eureka服务注册中心

--- eureka-config/src/main/java/com/huazheng/eureka/EurekaMainApplication.java
+++ eureka-config/src/main/java/com/huazheng/eureka/EurekaMainApplication.java
@@ -1,10 +1,19 @@
 package com.huazheng.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @Classname EurekaMainApplication
- * @Description TODO
+ * @Description 服务注册中心启动类
  * @Date 2020/7/2 13:39
  * @Created by zhanghuazheng
  */
+@SpringBootConfiguration
+@EnableEurekaServer
 public class EurekaMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMainApplication.class,args);
+    }
 }
