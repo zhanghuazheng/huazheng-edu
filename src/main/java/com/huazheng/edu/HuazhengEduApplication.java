f3be403d2b8a9e1b28b744bc7d0ad513fc882fe6
zhanghuazheng <zhanghzi@dicts.com>
2020-07-02 13:28:31 +0800

    项目初始化

---
+++ src/main/java/com/huazheng/edu/HuazhengEduApplication.java
@@ -0,0 +1,13 @@
+package com.huazheng.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuazhengEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuazhengEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huazheng/edu/HuazhengEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huazheng.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuazhengEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
