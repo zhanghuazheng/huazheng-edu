aef6c71d021538802d2bc3f88a4f0924a045aaa7
zhanghuazheng <zhanghzi@dicts.com>
2020-07-03 18:25:20 +0800

    1.springBoot集成mybatis-generatot插件

---
+++ common/src/main/java/com/huazheng/common/dao/TestMapper.java
@@ -0,0 +1,30 @@
+package com.huazheng.common.dao;
+
+import com.huazheng.common.domain.Test;
+import com.huazheng.common.domain.TestExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TestMapper {
+    long countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+}
